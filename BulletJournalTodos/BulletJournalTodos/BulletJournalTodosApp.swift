66c240b72481cdf6d5bf1842425b4d8b27c2f2ef
Vivian Qu <vivqu@vivian-personal.local>
2025-10-11 15:28:23 +0200

    Create starter Xcode project in SwiftUI

---
+++ BulletJournalTodos/BulletJournalTodos/BulletJournalTodosApp.swift
@@ -0,0 +1,32 @@
+//
+//  BulletJournalTodosApp.swift
+//  BulletJournalTodos
+//
+//  Created by Vivian Qu on 10/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BulletJournalTodosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
